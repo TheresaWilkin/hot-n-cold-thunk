6346f1f54b1d1aa5add9191525d997554829f665
iamromanh <romanh99@gmail.com>
2016-12-14 12:59:50 -0500

    write test for GUESS_NUMBER

--- test/test-index.js
+++ test/test-index.js
@@ -1,8 +1,9 @@
+/*jshint esversion: 6 */
 import React from 'react';
 import {createStore} from 'redux';
 
 import * as reducers from '../js/reducers/index';
-import * as actions from '../js/actions/index'
+import * as actions from '../js/actions/index';
 
 import TestUtils from 'react-addons-test-utils';
 import chai from 'chai';
@@ -10,20 +11,33 @@ const should = chai.should();
 
 
 const testStore = createStore(reducers.numberReducer);
+const number = 5;
 
 describe('New Game', function() {
   it('should create new game', function() {
-    const number = 5;
-    testStore.dispatch(actions.newGame(5));
+    testStore.dispatch(actions.newGame(number));
     let state = testStore.getState();
-    state.number.should.equal(5);
+    state.number.should.equal(number);
     state.guesses.length.should.equal(0);
     state.proximity.should.equal('Make a Guess!');
     state.won.should.equal(false);
   });
 });
 
-//{type:'GUESS_NUMBER',guess:'100'});
+describe('GUESS_NUMBER', function(){
+  it('should record a guess and respond appropriately', function() {
+    const guessedNumber = 100;
+    testStore.dispatch(actions.guessNumber(guessedNumber));
+    let state = testStore.getState();
+    state.number.should.equal(number);
+    state.currentGuess.should.equal(guessedNumber);
+    state.guesses.length.should.equal(1);
+    state.proximity.should.equal('ICE');
+    state.won.should.equal(false);
+  });
+});
+
+// {type:'GUESS_NUMBER',guess:'100'});
 //   expect(state).toEqual({number:5,currentGuess:'100',guesses:['100'],proximity:'ICE',won:false});
 //{type:'GUESS_NUMBER',guess:'20'});
 //   expect(state).toEqual({number:5,currentGuess:'20',guesses:['100','35','25','20'],proximity:'COLD',won:false});
@@ -33,6 +47,6 @@ describe('New Game', function() {
 //   expect(state).toEqual({number:5,currentGuess:'10',guesses:['100','35','25','20','15','10'],proximity:'VERY HOT',won:false});
 //{type:'GUESS_NUMBER',guess:'5'});
 //   expect(state).toEqual({number:5,currentGuess:'5',guesses:['100','35','25','20','15','10','5'],proximity:'WINNER',won:false});
-//   
-//   
-//   
+//
+//
+//
