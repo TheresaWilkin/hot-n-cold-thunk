269f6404f38b0aa391400c0616d8a41e2cdd8e26
Theresa Wilkin <theresawilkin@yahoo.com>
2016-12-15 12:45:09 -0500

    GET

---
+++ js/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// enable CORS since we're running two servers
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+var jsonParser = bodyParser.json();
+
+// you shouldn't need to server static since it is served by `npm run dev`
+// but keep here for reference
+//app.use(express.static('build'));
+
+let fewestGuesses = Infinity;
+app.get('/fewest-guesses', jsonParser, function(req, res) {
+    res.status(200).json(fewestGuesses);
+});
+
+app.post('/fewest-guesses', jsonParser, function(req, res) {
+	
+    // update fewestGuesses here
+    // remember to compare it to the current guesess to determine which is lowest
+});
+
+const PORT = 8081;
+app.listen(PORT, function () {
+    console.log('Example app listening on localhost:' + PORT);
+});
